aa52f90caee68e511da817f934385f872562dc2d
programmingrachel <81771817+programmingrachel@users.noreply.github.com>
2021-07-27 00:14:32 -0700

    add profile and team pages and css

--- playbook_sports/urls.py
+++ playbook_sports/urls.py
@@ -8,9 +8,13 @@ urlpatterns = [
     path('login', views.login),
     path('home', views.home),
     path('images', HomePageView.as_view(), name='images.html'),
-    path('<int:user_id>/profile',views.profile),
+    path('profile',views.profile),
+    path('team_schedule',views.team_schedule),
+    path('edit_account',views.edit_account),
+    path('<int:user_id>/update_profile',views.update_profile),
+    path('<int:team_id>/updatelogo',views.update_logo),
+    path('<int:team_id>/join',views.join),
+    path('<int:team_id>/remove',views.remove),
     path('logout',views.logout),
-    path('user/',CreateProfileView.as_view(), name='images')
-
 
 ]
--- playbook_sports/views.py
+++ playbook_sports/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
-from .forms import ProfileForm
+from .forms import ProfileForm, TeamForm
 from .models import *
+from django.db.models import Q
 import bcrypt
 import json
 import requests
@@ -47,22 +48,92 @@ def home(request):
     if 'user_id' not in request.session:
         return redirect("/")
     user = User.objects.get(id = request.session['user_id'])
+    userteams = Team.objects.filter(Q(captain = user) | Q(joined = user)).order_by("captain")
+    joinedteams = Team.objects.filter(joined = user)
     context = {
 		"user": user,
-		"user_teams":Team.objects.filter(captain = user),
-        "joined_teams" : Team.objects.filter(joined=user),
+		"user_teams":userteams,
+        "joined_teams" : joinedteams,
 		"other_teams": Team.objects.all().exclude(captain=user).exclude(joined=user),
 	}
     return render(request,"homepage.html",context) 
 
 
-def profile(request,user_id):
+def profile(request):
     user = User.objects.get(id = request.session['user_id'])
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.update(request.session['user_id'])
+            img_obj = form.instance
+            logoform = TeamForm
+            context={
+                'user': user,
+                "user_teams":Team.objects.filter(captain = user),
+                "form": form,
+                "logoform": logoform,
+                "img_obj": img_obj
+            }
+            return render(request,"profile.html",context)
+    else:
+        form = ProfileForm
+        logoform = TeamForm
+    
     context={
         'user': user,
-		"user_teams":Team.objects.filter(captain = user),
+        "user_teams":Team.objects.filter(captain = user),
+        "form": form,
+        "logoform": logoform
+        
     }
     return render(request, "profile.html",context)
+
+
+def update_profile(request, user_id):
+    user = User.objects.get(id = user_id)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.update(request.session['user_id'])
+            img_obj = form.instance
+            
+        return redirect("/profile")
+
+
+def team_schedule(request):
+    return render(request, "team_schedule.html")
+
+def edit_account(request):
+    return render(request, "update_profile.html")
+
+def update_logo(request,team_id):
+    user = User.objects.get(id = request.session['user_id'])
+    if request.method == 'POST':
+        logoform = TeamForm(request.POST, request.FILES)
+        if logoform.is_valid():
+            logoform.update(team_id)
+        return redirect("/profile")
+
+def join(request, team_id):
+    user = User.objects.get(id = request.session['user_id'])
+    joined_team = Team.objects.get(id= team_id)
+    joined_team.joined.add(user)
+    joined_team.save()
+    return redirect ('/home')
+
+def remove(request, team_id):
+    user = User.objects.get(id = request.session['user_id'])
+    joined_team = Team.objects.get(id= team_id)
+    joined_team.joined.remove(user)
+    joined_team.save()
+    return redirect ('/home')
+
+
+
+
+
 
 def logout(request):
     request.session.flush()
@@ -75,5 +146,10 @@ class HomePageView(ListView):
 class CreateProfileView(CreateView):
     model = User
     form_class = ProfileForm
-    template_name = 'user.html'
-    success_url=reverse_lazy('home')
+    template_name = 'profile.html'
+
+
+class CreateTeamView(CreateView):
+    model = Team
+    form_class = TeamForm
+    template_name = 'profile.html'
