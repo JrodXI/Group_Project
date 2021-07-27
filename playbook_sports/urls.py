27091efe1b1f644a29d660b5379b0af2367bda75
Marqus Shoels <mshoels07@gmail.com>
2021-07-27 11:21:42 -0500

    added create_team and team_rankings HTML

--- playbook_sports/urls.py
+++ playbook_sports/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from . import views
-from .views import HomePageView
+from .views import HomePageView, newteam
 
 urlpatterns = [
     path('', views.index),
     path('register', views.register),
     path('login', views.login),
-    path('home', views.home),
+    # path('home', views.home),
     # path('images', HomePageView.as_view(), name='images'),
+    path('create_team', views.createteam),
+    path('new_team', views.newteam),
+    path('team_rankings', views.teamrankings)
 ]
